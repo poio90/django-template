36d8b593193249bf913c5569e864bb15e310fa89
German Vargas <germanv09@gmail.com>
2025-03-17 17:15:14 -0300

    first commit

---
+++ app/config/settings/dev.py
@@ -0,0 +1,33 @@
+import os
+
+from .base import *
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG", True)
+
+ALLOWED_HOSTS = ["*"]
+
+THIRD_PARTY_APP = [
+    "django_extensions",
+]  # third party apps goes here
+
+INSTALLED_APPS = INSTALLED_APPS + THIRD_PARTY_APP
+
+# If you want to use postgresql instead, then uncomment this and comment that of sqlite3 bellow.
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": os.getenv("DB_NAME"),
+#         "USER": os.getenv("DB_USER"),
+#         "PASSWORD": os.getenv("DB_PWD"),
+#         "HOST": os.getenv("DB_HOST"),
+#         "PORT": os.getenv("DB_PORT"),
+#     }
+# }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
