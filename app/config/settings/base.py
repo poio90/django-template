71e09c2a9b2d953fa261ac710ad86badbbbac736
German Vargas <germanv09@gmail.com>
2025-03-21 17:50:01 -0300

    refactor(backend): se agrega multilanguage support

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,6 +29,7 @@ INSTALLED_APPS = [*DEFAULT_APP, *CREATED_APP, *THIRD_PARTY_APP]
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'django.middleware.locale.LocaleMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -97,6 +99,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+USE_L10N = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
@@ -107,3 +111,54 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LANGUAGES = [
+    # ("ar", _("Arabic")),
+    # ("az", _("Azerbaijani")),
+    # ("bg", _("Bulgarian")),
+    # ("ca", _("Catalan")),
+    # ("cs", _("Czech")),
+    # ("da", _("Danish")),
+    # ("de", _("German")),  #
+    # ("el", _("Greek")),
+    ("en", _("English")),  #
+    ("es", _("Spanish")),  #
+    # ("et", _("Estonian")),
+    # ("eu", _("Basque")),
+    # ("fa", _("Persian")),
+    # ("fi", _("Finnish")),
+    # ("fr", _("French")),  #
+    # ("he", _("Hebrew")),
+    # ("hr", _("Croatian")),
+    # ("hu", _("Hungarian")),
+    # ("id", _("Indonesian")),
+    # ("it", _("Italian")),  #
+    # ("ja", _("Japanese")),  #
+    # ("ka", _("Georgian")),
+    # ("ko", _("Korean")),  #
+    # ("ky", _("Kyrgyz")),
+    # ("lt", _("Lithuanian")),
+    # ("lv", _("Latvian")),
+    # ("mn", _("Mongolian")),
+    # ("nb", _("Norwegian Bokmål")),
+    # ("nl", _("Dutch")),
+    # ("pl", _("Polish")),
+    # ("pt-BR", _("Portuguese (Brazil)")),  #
+    # ("pt-PT", _("Portuguese (Portugal)")),
+    # ("ro", _("Romanian")),
+    # ("ru", _("Russian")),
+    # ("sk", _("Slovak")),
+    # ("sl", _("Slovenian")),
+    # ("sr", _("Serbian")),
+    # ("sr-Latn", _("Serbian (Latin)")),
+    # ("sv", _("Swedish")),
+    # ("th", _("Thai")),
+    # ("tr", _("Turkish")),
+    # ("uk", _("Ukrainian")),
+    # ("zh-hans", _("Chinese (Simplified)")),  #
+    # ("zh-hant", _("Chinese (Traditional)")),
+]
+
+LOCALE_PATHS = [
+    BASE_DIR / 'locale/',
+]
